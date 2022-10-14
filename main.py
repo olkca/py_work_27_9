52ab8856a1c168471379a2b8e7a08d9a75e91523
Shapoval <olekcandrsapoval41@gmail.com>
2022-10-14 22:52:05 +0300

    Ex_3

---
+++ main.py
@@ -0,0 +1,11 @@
+metr = int(input(""))
+mode = int(input(""))
+if mode == 1:
+    mili = metr * 0.000621371
+    print(mili)
+if mode == 2:
+    dyumu = metr * 39.37
+    print(dyumu)
+if mode == 3:
+    audu = metr / 1.0936132983377
+    print(audu)
