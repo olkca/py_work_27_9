b05e297a522fd46930f9153a09c874a40d7a05e8
Shapoval <olekcandrsapoval41@gmail.com>
2022-10-14 22:32:16 +0300

    Ex_1

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = int(input(""))
+num2 = int(input(""))
+num3 = int(input(""))
+mode = int(input(""))
+if mode == 1:
+    print(num1 + num2 + num3)
+if mode == 2:
+    print(num1 * num2 * num3)
